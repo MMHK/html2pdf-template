062fffe38f3a1c73439fe5d0850c2d581fd0d8cf
SamZhou <sam@mixmedia.com>
2018-02-22 17:32:56 +0800

    add more function for generate PDF file.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # html2pdf-template
 The template for html2pdf project
 
+## 初始化
+
+`npm install`
+
 ## 嵌入字体
 
 - 请将 `ttf` 字体文件放到 `src/fonts/` 目录下，然后执行 `gulp` 进行字体的裁剪/嵌入。
@@ -9,3 +13,12 @@ The template for html2pdf project
 
 - 安装 `create-project`， `npm install -g create-project`
 - 导出项目模板， `create-project [你的新项目名称] MMHK/html2pdf-template`
+
+## 发布外网访问url
+
+- `gulp public`, 执行后会自动打开浏览器访问地址。
+
+## 生成PDF
+
+- 打开 [PDF生成内网版](http://192.168.33.126:4444/sample/)
+- 找到 `Covert Link to PDF` ， 将上一步生成的URL copy到输入框，生成pdf。
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,41 @@
 var gulp = require("gulp");
 var fontmin = require("gulp-fontmin");
+var GulpSSH = require('gulp-ssh');
+var open = require('gulp-open');
 
+
+var dir = {
+    path: Date.now()
+};
+var remote = "http://pdf.demo2.mixmedia.com/" + dir.path;
+
+var config = {
+    host: '192.168.33.6',
+    port: 222,
+    username: 'temp',
+    password : 'temp',
+    prefix: "/webroot/" + dir.path + "/"
+};
+
+var gulpSSH = new GulpSSH({
+    ignoreErrors: false,
+    sshConfig: config
+});
+
+gulp.task('public:html', function () {
+    return gulp.src('./*.html')
+        .pipe(gulpSSH.dest(config.prefix))
+});
+
+gulp.task('public:fonts', function () {
+    return gulp.src('./fonts/*.*')
+        .pipe(gulpSSH.dest(config.prefix + "fonts/"))
+});
+
+gulp.task('public:css', function () {
+    return gulp.src('./*.css')
+        .pipe(gulpSSH.dest(config.prefix))
+});
 
 function minifyFont(text, cb) {
     gulp
@@ -26,6 +61,10 @@ gulp.task('font', function(cb) {
             minifyFont(text, cb);
         });
  
+});
+gulp.task("public", ["public:html", "public:fonts", "public:css"], function () {
+    gulp.src(__filename)
+        .pipe(open({uri: remote}));
 });
 
 gulp.task("default", ["font"]);
